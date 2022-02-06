d9a357ac8046fdafdf66e987d8b60f52ba4e8e47
ebrahim78652 <ebrahimkarjatwala@gmail.com>
2022-02-06 12:35:56 +0100

    completed functionality of the calculator

--- javascript.js
+++ javascript.js
@@ -2,35 +2,60 @@ console.log("hello!")
 //(number)=>number.addEventListener('click',()=>console.log("numberPressed!"))
 
 //add a listener to all of the buttons. This//when they are pressed: print button pressed!
-let firstnumberPressed=0;
-let allNumbers= document.querySelectorAll(".number");
-allNumbers.forEach((number)=>number.addEventListener('click', (event)=>{
+let firstnumberPressed = 0;
+let allNumbers = document.querySelectorAll(".number");
+allNumbers.forEach((number) => number.addEventListener('click', (event) => {
     console.log(`the number pressed is: ${parseInt(number.id)}`);
-    firstnumberPressed= parseInt(number.id);
-    let display= document.querySelector(".numbers");
-    display.innerHTML=display.innerHTML+firstnumberPressed;
+    firstnumberPressed = parseInt(number.id);
+    let display = document.querySelector(".numbers");
+    display.innerHTML = display.innerHTML + firstnumberPressed;
 }))
 
 //add a listener to the operation buttons
-let operationPressed="";
-let allOperations= document.querySelectorAll(".operation");
-allOperations.forEach((operation)=>operation.addEventListener('click', (event)=>{
+let operationPressed = "";
+let allOperations = document.querySelectorAll(".operation");
+allOperations.forEach((operation) => operation.addEventListener('click', (event) => {
     console.log(`the operation pressed is: ${(operation.id)}`);
-    operationPressed= operation.id;
-    let display= document.querySelector(".numbers");
-    display.innerHTML=display.innerHTML+ operationPressed;
+    operationPressed = operation.id;
+    let display = document.querySelector(".numbers");
+    display.innerHTML = display.innerHTML + operationPressed;
 }))
+
+//function to display the answer on the calculator
+displayAnswer = function (answer) {
+    let display = document.querySelector(".numbers");
+    display.innerHTML = answer;
+}
 
 //add a listener to the calculateButton 
 
-let operationCalculate= document.querySelector(".operationCalculate");
-operationCalculate.addEventListener('click', (event)=>{
+let operationCalculate = document.querySelector(".operationCalculate");
+operationCalculate.addEventListener('click', (event) => {
     console.log("calulate button pressed");
-    let display= document.querySelector(".numbers");
-    let calculationEnteredByTheUser= display.innerHTML;
-    let temp= calculationEnteredByTheUser.split('+');
-    console.log(temp);
-    console.log(operationPressed)
+    let display = document.querySelector(".numbers");
+    let calculationEnteredByTheUser = display.innerHTML;
+    let operands = calculationEnteredByTheUser.split(operationPressed);
+    console.log(operands[0]);
+    console.log(operands[1]);
+    console.log(operationPressed);
+    //convert the strings into numbers, and then make a switch case and update the dom.
+
+    let firstOperandConvertedToNumber = parseInt(operands[0]);
+    let secondOperandConvertedToNumber = parseInt(operands[1]);
+
+    switch (operationPressed) {
+        case '+':
+            displayAnswer(firstOperandConvertedToNumber + secondOperandConvertedToNumber)
+            break;
+        case '-':
+            displayAnswer( firstOperandConvertedToNumber - secondOperandConvertedToNumber)
+            break;
+        case 'x':
+            displayAnswer(firstOperandConvertedToNumber * secondOperandConvertedToNumber)
+            break;
+        default:
+            console.log(`Sorry, we are out of ${expr}.`);
+    }
 
 
 })
